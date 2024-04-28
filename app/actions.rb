7c34c93ce52a813dd5234782a3937c383a5e58b1
ColinLHL2 <colinlhl2@proton.me>
2024-04-28 22:37:26 +0000

    add get and post signup routes

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,39 @@
-# Handle incoming HTTP request and send HTTP response
+# Controller
+
+# Specify our routes/actions
+
+# Handle the GET request for the path '/'
 get '/' do
-  # get FPosts from database (array of object)
-  @finstagram_posts = FinstagramPost.order(created_at: :desc)  
-  # use Sinatra erb method to use a View file
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
 
+# Handle the GET request for the path '/signup'
+get '/signup' do
+  @user = User.new
+  erb(:signup)
+end
 
+# Handle the POST request for the path '/signup'
+post '/signup' do
+  # handle form data from request body
+  email = params[:email]
+  avatar_url = params[:avatar_url]
+  username = params[:username]
+  password = params[:password]
+
+  # instantiate a User
+  @user = User.new({
+    email: email, 
+    avatar_url: avatar_url, 
+    username: username, 
+    password: password 
+  })
+
+  # create a user record
+  if @user.save
+    "Signed up username: #{@user.username}"
+  else
+    erb(:signup)
+  end
+end
