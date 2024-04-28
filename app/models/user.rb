ad2ed51b9fd72d924432d680daddbb4d3427e718
ColinLHL2 <colinlhl2@proton.me>
2024-04-28 22:38:26 +0000

    implement signup feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :likes
 
+  # validation methods
+  validates(:email, :username, uniqueness: true)
+  validates(:email, :username, :avatar_url, :password, presence: true)
+
 end
